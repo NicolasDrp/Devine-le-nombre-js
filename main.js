b60c8b41facd9fbd4f5df20cb1099568c2218e53
NicolasDrp <drapier.nicolas07@gmail.com>
2023-01-28 21:50:49 +0100

    changer alert en html

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 const nombreadeviner = 15;
 let choixjoueur;
 
+const message = document.createElement("p");
 
+document.getElementById("form").addEventListener("submit", reponse);
 
-function checkAnswer(event) {
+function reponse(event) {
     event.preventDefault();
     choixjoueur = document.getElementById("nombre").value;
-    if (nombreadeviner == choixjoueur) {
-        alert("c'est bon");
-    }else if(choixjoueur > nombreadeviner) {
-        alert("c'est moins");
-    }else{
-        alert("c'est plus");
-    }
 
+
+    if (nombreadeviner == choixjoueur) {
+        message.innerHTML = "Moins";
+    } else if (choixjoueur > nombreadeviner) {
+        message.innerHTML = "Moins";
+    } else {
+        message.innerHTML = "Plus";
+    }
+    document.getElementById("parent").appendChild(message);
 }
 
-document.getElementById("form").addEventListener("submit", checkAnswer);
+
 
